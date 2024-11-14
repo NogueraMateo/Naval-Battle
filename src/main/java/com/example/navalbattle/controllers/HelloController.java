6f1a1dd176ebc7c3f2eb3168aedf2dc3426f9ba8
Juanctoro <juancarlostorocifuentes12@gmail.com>
2024-11-14 12:09:02 -0500

    First version of the game's home-welcome view

--- src/main/java/com/example/navalbattle/controllers/HelloController.java
+++ src/main/java/com/example/navalbattle/controllers/HelloController.java
@@ -5,7 +5,9 @@ import com.example.navalbattle.views.GameView;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,14 +16,23 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
-    private GameModel gameModel = new GameModel();
+    @FXML
+    private TextField inputUsername;
 
     @FXML
-    protected void onHelloButtonClick(Event event) throws IOException {
-        Node source = (Node) event.getSource();
-        Stage actualStage = (Stage) source.getScene().getWindow();
-        actualStage.close();
+    protected void onHelloButtonPlay(Event event) throws IOException {
+        if (inputUsername == null || inputUsername.getText().trim().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter your username!");
+            alert.showAndWait();
+        } else {
+            Node source = (Node) event.getSource();
+            Stage actualStage = (Stage) source.getScene().getWindow();
+            actualStage.close();
 
-        GameView.getInstance();
+            GameView.getInstance();
+        }
     }
 }
---
+++ src/main/java/com/example/navalbattle/views/HelloView.java
@@ -0,0 +1,51 @@
+package com.example.navalbattle.views;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.text.View;
+import java.io.IOException;
+
+public class HelloView extends Stage {
+    /**
+     * Initializes the GameView by loading the FXML layout and setting
+     * the window's title, scene, and other properties.
+     *
+     * @throws IOException if there is an issue loading the FXML file
+     */
+    public HelloView() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/navalbattle/hello-view.fxml"));
+        Parent root = loader.load();
+        this.setTitle("Naval Battle");
+        Scene scene = new Scene(root);
+        this.setScene(scene);
+        this.setResizable(false);
+        this.show();
+    }
+
+    /**
+     * Holds the single instance of GameView, following the Singleton pattern.
+     */
+    private static class HelloViewHolder {
+        private static HelloView INSTANCE;
+    }
+
+    /**
+     * Provides access to the single instance of GameView.
+     * If the instance does not exist, it is created; otherwise, the existing
+     * instance is returned.
+     *
+     * @return the singleton instance of GameView
+     * @throws IOException if there is an issue loading the FXML file
+     */
+    public static HelloView getInstance() throws IOException {
+
+        if (HelloViewHolder.INSTANCE == null) {
+            return HelloViewHolder.INSTANCE = new HelloView();
+        } else {
+            return HelloViewHolder.INSTANCE;
+        }
+    }
+}
